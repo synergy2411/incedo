b903c8128fc26dad63129bcf3198e8b49d17d06d
synergy2411 <synergy2411@outlook.com>
2019-11-19 15:05:58 +0530

    api added

--- playground/express-demo/frontend-demo.js
+++ playground/express-demo/frontend-demo.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
 app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json({
+    type : "*/*"
+}));
 
 app.get("/", (req, res)=>{
     // res.send("<h1>Some cool heading</h1>")
@@ -9,6 +15,25 @@ app.get("/", (req, res)=>{
     // res.send({
     //     status : "done"
     // })
+})
+
+app.get("/login", (req, res)=>{
+    if(req.query.username && req.query.password ){
+        console.log("Username : ", req.query.username)
+        console.log("Password : ", req.query.password)
+        return res.send({"status" : "success"})
+    }
+    res.redirect("/");
+})
+
+app.post("/login", (req, res) => {
+    if(req.body){
+        console.log(req.body)
+        // console.log("Username : ", req.body.username)
+        // console.log("Password : ", req.body.password)
+        return res.send({"status" : "success"})
+    }
+    res.redirect("/");
 })
 
 app.listen(9090, () => {
