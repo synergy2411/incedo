866f9bad4423795cf0d5fe391795dc1fd97115f7
synergy2411 <synergy2411@outlook.com>
2019-11-26 11:31:20 +0530

    structural directive added

--- client/projects/users/src/app/model/mocks.ts
+++ client/projects/users/src/app/model/mocks.ts
@@ -1,6 +1,6 @@
 import { User } from "./user";
 
-export const USER_DATA: User = {
+export const USER_DATA: User[] = [{
   firstName: "Bill",
   lastName: "Gates",
   dob: new Date('Dec 16, 1965'),
@@ -9,4 +9,22 @@ export const USER_DATA: User = {
   isWorking: true,
   image: "assets/images/bill.jpg",
   vote: 120
-}
+}, {
+  firstName: "Steve",
+  lastName: "Jobs",
+  dob: new Date('Jan 1, 1965'),
+  company: 'MicrosoftApple',
+  income: 0,
+  isWorking: false,
+  image: "assets/images/steve.png",
+  vote: 100
+},{
+  firstName: "Tim B.",
+  lastName: "Lee",
+  dob: new Date('Feb 6, 1965'),
+  company: 'World Wide Web',
+  income: 30000,
+  isWorking: true,
+  image: "assets/images/tim.jpg",
+  vote: 180
+}]
--- client/projects/users/src/app/users/users.component.ts
+++ client/projects/users/src/app/users/users.component.ts
@@ -18,12 +18,15 @@ import { USER_DATA} from '../model/mocks';
 export class UsersComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
   @Input() title : string;
 
-  user : User;
+  users : User[];
 
   onMoreInfo(user : User){
     alert(`${user.firstName} is working with ${user.company}!` );
   }
 
+  trackByFn(index, user){
+    return index;
+  }
   constructor(){
     // this.user = USER_DATA;
   }
@@ -34,7 +37,7 @@ export class UsersComponent implements OnChanges, OnInit, DoCheck, AfterContentI
 
   ngOnInit(){
     console.log("ngOnInit");
-    this.user = USER_DATA;
+    this.users = USER_DATA;
   }
   ngDoCheck(){console.log("DoCheck");}
   ngAfterContentInit(){console.log("ngAfterContentInit");}
@@ -42,5 +45,64 @@ export class UsersComponent implements OnChanges, OnInit, DoCheck, AfterContentI
   ngAfterViewInit(){console.log("ngAfterViewInit");}
   ngAfterViewChecked(){console.log("ngAfterViewChecked");}
   ngOnDestroy(){console.log("ngOnDestroy");}
+
+  addNewUser(){
+    this.users = [{
+      firstName: "Bill",
+      lastName: "Gates",
+      dob: new Date('Dec 16, 1965'),
+      company: 'Microsoft',
+      income: 50000,
+      isWorking: true,
+      image: "assets/images/bill.jpg",
+      vote: 120
+    }, {
+      firstName: "Steve",
+      lastName: "Jobs",
+      dob: new Date('Jan 1, 1965'),
+      company: 'MicrosoftApple',
+      income: 0,
+      isWorking: false,
+      image: "assets/images/steve.png",
+      vote: 100
+    },{
+      firstName: "Tim B.",
+      lastName: "Lee",
+      dob: new Date('Feb 6, 1965'),
+      company: 'World Wide Web',
+      income: 30000,
+      isWorking: true,
+      image: "assets/images/tim.jpg",
+      vote: 180
+    },{
+      firstName: "Bill",
+      lastName: "Gates",
+      dob: new Date('Dec 16, 1965'),
+      company: 'Microsoft',
+      income: 50000,
+      isWorking: true,
+      image: "assets/images/bill.jpg",
+      vote: 120
+    }, {
+      firstName: "Steve",
+      lastName: "Jobs",
+      dob: new Date('Jan 1, 1965'),
+      company: 'MicrosoftApple',
+      income: 0,
+      isWorking: false,
+      image: "assets/images/steve.png",
+      vote: 100
+    },{
+      firstName: "Tim B.",
+      lastName: "Lee",
+      dob: new Date('Feb 6, 1965'),
+      company: 'World Wide Web',
+      income: 30000,
+      isWorking: true,
+      image: "assets/images/tim.jpg",
+      vote: 180
+    }]
+
+  }
 
 }
